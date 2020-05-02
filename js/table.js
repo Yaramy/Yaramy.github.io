8c431e3a9c78ae7f2b0a8a807dd6998cef0b952a
Yaramy <64393005+Yaramy@users.noreply.github.com>
2020-05-02 17:59:39 -0500

    bigger table

--- js/table.js
+++ js/table.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "400px";
+  document.getElementById("main").style.marginLeft = "400px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
